f672c5271d9b33030c8718fe3fd18ab672aec499
BanuDarius <darius-matei.banu@s.unibuc.ro>
2026-04-06 22:09:41 +0300

    Clean directory

--- build/hyper_vlasov.d
+++
@@ -1,8 +0,0 @@
-build/hyper_vlasov.o: src/hyper_vlasov.c include/init.h \
- include/sim_structs.h include/tools.h include/physics.h \
- include/sim_structs.h
-include/init.h:
-include/sim_structs.h:
-include/tools.h:
-include/physics.h:
-include/sim_structs.h:
--- build/init.d
+++
@@ -1,9 +0,0 @@
-build/init.o: src/init.c include/init.h include/sim_structs.h \
- include/tools.h include/physics.h include/math_tools.h \
- include/sim_structs.h
-include/init.h:
-include/sim_structs.h:
-include/tools.h:
-include/physics.h:
-include/math_tools.h:
-include/sim_structs.h:
