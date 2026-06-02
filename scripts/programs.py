49613089f5bcb2716cf31e5ad874b0d963c473a3
BanuDarius <darius-matei.banu@s.unibuc.ro>
2026-06-02 14:10:06 +0300

    Added detrend

--- scripts/programs.py
+++ scripts/programs.py
@@ -24,6 +24,7 @@ SOFTWARE.'''
 
 import numpy as np
 from scipy.fft import fft, fftfreq
+from scipy.signal import detrend
 import subprocess
 from pathlib import Path
 
@@ -100,7 +101,7 @@ def compute_energy_spectrum(sim_parameters):
     cm_neutrons = data[start_idx:, 4]
     
     dipole = (n * z) / (n + z) * (cm_protons - cm_neutrons)
-    dipole -= dipole[0]
+    dipole = detrend(dipole, type='linear')
     
     t0 = time[0]
     dt = t_f / steps * substeps
