1f8ef13d855c3a54656625ff549348e92b25247b
BanuDarius <darius-matei.banu@s.unibuc.ro>
2026-06-08 03:37:16 +0300

    Updated core binding

--- slurm.sh
+++ slurm.sh
@@ -8,8 +8,8 @@
 #SBATCH --mem=256GB
 #SBATCH --time=2:00:00
 
-export OMP_PLACES=threads
-export OMP_PROC_BIND=spread
+export OMP_PLACES=cores
+export OMP_PROC_BIND=true
 
 source $HOME/python-env/bin/activate
 
